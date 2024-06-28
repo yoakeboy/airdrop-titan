3d598fc3bb6d865990831a27abcc7f171c10486b
YoakeBoy <153350566+yoakeboy@users.noreply.github.com>
2024-06-28 19:49:40 +0700

    Update titan3.sh

--- titan3.sh
+++ titan3.sh
@@ -6,7 +6,7 @@ if [ "$(id -u)" != "0" ]; then
     echo "Silakan coba gunakan perintah 'sudo -i' untuk beralih ke pengguna root, lalu jalankan skrip ini lagi."
     exit 1
 fi
-
+clear
 echo "Saya cuma modif source code"
 echo "Telegram @nugz5"
 echo "=========================Titan Node================================"
@@ -14,7 +14,7 @@ echo "=========================Titan Node================================"
 # Minta input dari pengguna
 read -p "Masukkan Code Identity Anda: " id
 read -p "Masukkan Size Disk GB (misalnya 100): " storage
-
+clear
 # Update paket repository
 apt update
 
@@ -26,6 +26,7 @@ if ! command -v docker &> /dev/null; then
     # Instal Docker versi terbaru
     apt-get install docker.io -y
 else
+    clear
     echo "Docker telah diinstal."
 fi
 
@@ -37,6 +38,7 @@ mkdir -p ~/.titanedge
 
 # Jalankan container dan simpan ID kontainer yang dibuat
 container_id=$(docker run --network=host -d -v ~/.titanedge:/root/.titanedge nezha123/titan-edge)
+clear
 echo "Node Titan telah sukses dibuat"
 
 # Tunggu beberapa saat untuk memastikan kontainer siap
@@ -48,7 +50,7 @@ docker exec $container_id titan-edge config set --listen-address 0.0.0.0:9000
 
 # Masuk ke dalam container dan lakukan pengikatan Order
 docker run --rm -it -v ~/.titanedge:/root/.titanedge nezha123/titan-edge bind --hash=$id https://api-test1.container1.titannet.io/api/v2/device/binding
-
+clear
 # Mulai ulang kontainer agar pengaturan diterapkan
 echo "ID CONTAINER"
 docker restart $container_id
@@ -62,6 +64,7 @@ echo "=========================SHOW NODE INFO============================="
 docker exec $container_id titan-edge info
 sleep 5
 echo "====================NODE TELAH SUKSES BERJALAN======================"
+clear
 
 # Menambahkan pilihan untuk melihat log atau keluar dari skrip
 read -p "Apakah Anda ingin melihat log container? (ketik '1' untuk melihat log, atau tekan enter untuk keluar): " choice
