71713e0f7b1658aee244ffd5823f327ccad0833c
YoakeBoy <153350566+yoakeboy@users.noreply.github.com>
2024-06-28 19:50:59 +0700

    Update titan3.sh

--- titan3.sh
+++ titan3.sh
@@ -6,7 +6,9 @@ if [ "$(id -u)" != "0" ]; then
     echo "Silakan coba gunakan perintah 'sudo -i' untuk beralih ke pengguna root, lalu jalankan skrip ini lagi."
     exit 1
 fi
-clear
+
+printf "\033c"  # Membersihkan terminal
+
 echo "Saya cuma modif source code"
 echo "Telegram @nugz5"
 echo "=========================Titan Node================================"
@@ -14,7 +16,9 @@ echo "=========================Titan Node================================"
 # Minta input dari pengguna
 read -p "Masukkan Code Identity Anda: " id
 read -p "Masukkan Size Disk GB (misalnya 100): " storage
-clear
+
+printf "\033c"  # Membersihkan terminal
+
 # Update paket repository
 apt update
 
@@ -26,7 +30,7 @@ if ! command -v docker &> /dev/null; then
     # Instal Docker versi terbaru
     apt-get install docker.io -y
 else
-    clear
+    printf "\033c"  # Membersihkan terminal
     echo "Docker telah diinstal."
 fi
 
@@ -38,7 +42,8 @@ mkdir -p ~/.titanedge
 
 # Jalankan container dan simpan ID kontainer yang dibuat
 container_id=$(docker run --network=host -d -v ~/.titanedge:/root/.titanedge nezha123/titan-edge)
-clear
+
+printf "\033c"  # Membersihkan terminal
 echo "Node Titan telah sukses dibuat"
 
 # Tunggu beberapa saat untuk memastikan kontainer siap
@@ -50,21 +55,28 @@ docker exec $container_id titan-edge config set --listen-address 0.0.0.0:9000
 
 # Masuk ke dalam container dan lakukan pengikatan Order
 docker run --rm -it -v ~/.titanedge:/root/.titanedge nezha123/titan-edge bind --hash=$id https://api-test1.container1.titannet.io/api/v2/device/binding
-clear
-# Mulai ulang kontainer agar pengaturan diterapkan
+
+printf "\033c"  # Membersihkan terminal
 echo "ID CONTAINER"
 docker restart $container_id
 echo "Node Titan berhasil terikat dengan IDENTITY CODE."
 
 sleep 10
+
+printf "\033c"  # Membersihkan terminal
 echo "===========================SHOW CONFIG=============================="
 docker exec $container_id titan-edge config show
+
 sleep 10
+
+printf "\033c"  # Membersihkan terminal
 echo "=========================SHOW NODE INFO============================="
 docker exec $container_id titan-edge info
+
 sleep 5
+
+printf "\033c"  # Membersihkan terminal
 echo "====================NODE TELAH SUKSES BERJALAN======================"
-clear
 
 # Menambahkan pilihan untuk melihat log atau keluar dari skrip
 read -p "Apakah Anda ingin melihat log container? (ketik '1' untuk melihat log, atau tekan enter untuk keluar): " choice
@@ -72,9 +84,11 @@ read -p "Apakah Anda ingin melihat log container? (ketik '1' untuk melihat log, 
 if [ "$choice" = "1" ]; then
     # Membuat session tmux dengan nama "titan" dan menjalankan perintah untuk melihat log
     tmux new-session -d -s titan "docker logs -f -t $container_id"
+    printf "\033c"  # Membersihkan terminal
     echo "Sesi 'titan' telah dibuat untuk melihat log container."
     echo "Anda dapat masuk ke sesi dengan menjalankan 'tmux attach-session -t titan'."
     echo "Untuk keluar dari sesi tekan 'CTRL + b' lalu tekan 'd'"
 else
+    printf "\033c"  # Membersihkan terminal
     echo "Terima kasih telah menggunakan skrip ini. Sampai jumpa!"
 fi
